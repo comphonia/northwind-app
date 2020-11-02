d4bf5d6058efe9b2f9771954c3ee53b5dee38d50
comphonia <online.comphonia@gmail.com>
2020-11-01 22:16:22 -0600

    application bootstrap using ASP.NET Core MVC

---
+++ NorthwindApp/NorthwindWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NorthwindWeb.Data;
+
+[assembly: HostingStartup(typeof(NorthwindWeb.Areas.Identity.IdentityHostingStartup))]
+namespace NorthwindWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
